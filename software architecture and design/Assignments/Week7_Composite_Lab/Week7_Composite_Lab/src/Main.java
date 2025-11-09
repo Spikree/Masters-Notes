68155a959b95c7e835354669642eeccbffd82dc1
Spikree <avimahalingpure10@gmail.com>
2025-11-09 23:05:31 +0000

    week 7 lab

---
+++ software architecture and design/Assignments/Week7_Composite_Lab/Week7_Composite_Lab/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Create individual shapes
+        System.out.println("Hello World!");
+
+        // TODO: Create shape groups and add shapes to them
+
+        // TODO: Draw the entire structure
+    }
+}
